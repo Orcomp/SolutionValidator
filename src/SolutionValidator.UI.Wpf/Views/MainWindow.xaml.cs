8f522540f36a06896331093b412103d1f8baad27
Marko Vermeer <marko.vermeer@mc-solutions.nl>
2014-04-14 19:05:10 +0200

    Added mainWindow.xaml.cs

---
+++ src/SolutionValidator.UI.Wpf/Views/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using Catel.Windows;
+
+namespace SolutionValidator.UI.Wpf.Views
+{
+	/// <summary>
+	///     Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+	    public MainWindow()
+            : base(DataWindowMode.Custom)
+		{
+			InitializeComponent();
+            SizeToContent = SizeToContent.Manual;
+		}
+        
+	    protected override void OnClosed(EventArgs e)
+	    {
+	        base.OnClosed(e);
+
+            ViewModel.CloseViewModel(null);
+	    }
+	}
+}
