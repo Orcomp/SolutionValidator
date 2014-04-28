5565b88dd96439b72a41f51d4d451539602f2074
Marko Vermeer <marko.vermeer@mc-solutions.nl>
2014-04-28 16:12:34 +0200

    Added missing files

---
+++ src/SolutionValidator.Core/Configuration/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,84 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConfigurationExtensions.cs" company="Orcomp development team">
+//   Copyright (c) 2008 - 2014 Orcomp development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SolutionValidator.Configuration
+{
+    using Catel;
+    using Models;
+
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Create a <see cref="ValidatorContext"/> instance from a <see cref="ValidatorContext"/>.
+        /// </summary>
+        /// <param name="configuration">The configuration instance.</param>
+        /// <returns>A new <see cref="ValidatorContext"/> instance.</returns>
+        public static ValidatorContext ToContext(this SolutionValidatorConfigurationSection configuration)
+        {
+            Argument.IsNotNull(() => configuration);
+
+            var validatorContext = new ValidatorContext();
+
+            if (configuration.FolderStructure != null)
+            {
+                CreateFolderStructureContext(configuration, validatorContext);
+            }
+
+            if (configuration.ProjectFile != null)
+            {
+                CreateProjectFileContext(configuration, validatorContext);
+            }
+            
+            return validatorContext;
+        }
+
+        private static void CreateFolderStructureContext(SolutionValidatorConfigurationSection configuration, ValidatorContext validatorContext)
+        {
+            Argument.IsNotNull(() => configuration);
+            Argument.IsNotNull(() => validatorContext);
+
+            validatorContext.FolderStructure.DefinitionFilePath = configuration.FolderStructure.DefinitionFilePath;
+            validatorContext.FolderStructure.Check = configuration.FolderStructure.Check;
+        }
+
+        private static void CreateProjectFileContext(SolutionValidatorConfigurationSection configuration, ValidatorContext validatorContext)
+        {
+            Argument.IsNotNull(() => configuration);
+            Argument.IsNotNull(() => validatorContext);
+
+            validatorContext.ProjectFile.CheckOutPutPath = configuration.ProjectFile.OutputPath.Check;
+            validatorContext.ProjectFile.OutputPath = configuration.ProjectFile.OutputPath.Value;
+            validatorContext.ProjectFile.CheckRequiredConfigurations = configuration.ProjectFile.RequiredConfigurations.Check;
+            validatorContext.ProjectFile.CheckIdentical = configuration.ProjectFile.CheckIdentical.Check;
+            validatorContext.ProjectFile.CheckPropertyValues = configuration.ProjectFile.CheckForValue.Check;
+
+            if (configuration.ProjectFile.RequiredConfigurations != null)
+            {
+                foreach (ConfigurationNameElement configurationNameElement in configuration.ProjectFile.RequiredConfigurations)
+                {
+                    validatorContext.ProjectFile.RequiredConfigurations.Add(configurationNameElement.Name);
+                }
+            }
+
+            if (configuration.ProjectFile.CheckIdentical != null)
+            {
+                foreach (PropertiesToMatchElement propertiesToMatchElement in configuration.ProjectFile.CheckIdentical)
+                {
+                    validatorContext.ProjectFile.IdenticalChecks.Add(new IdenticalCheck() 
+                        {PropertyName = propertiesToMatchElement.PropertyName, OtherPropertyName = propertiesToMatchElement.OtherPropertyName});
+                }
+            }
+
+            if (configuration.ProjectFile.CheckForValue != null)
+            {
+                foreach (PropertyToCheckElement propertyToCheckElement in configuration.ProjectFile.CheckForValue)
+                {
+                    validatorContext.ProjectFile.Properties.Add(new Property() {Name = propertyToCheckElement.PropertyName, Value = propertyToCheckElement.Value});
+                }
+            }            
+        }        
+    }
+}
---
+++ src/SolutionValidator.Core/Models/Context.cs
@@ -0,0 +1,114 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Context.cs" company="Orcomp development team">
+//   Copyright (c) 2008 - 2014 Orcomp development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SolutionValidator
+{
+    using System;
+    using System.IO;
+    using Catel;
+    using Catel.Data;
+    using Configuration;
+    using Models;
+
+    public class Context : ModelBase
+    {
+        #region
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Context"/> class.
+        /// </summary>
+        /// <param name="repositoryRootPath">The repository root path.</param>
+        /// <param name="configFilePath">The configuration file path.</param>
+        public Context(string repositoryRootPath, string configFilePath = null)
+        {
+            Argument.IsNotNullOrWhitespace(() => repositoryRootPath);
+
+            RepositoryRootPath = repositoryRootPath;
+            ConfigFilePath = configFilePath;
+
+            ValidateContext();
+
+            var validatorContext = CreateValidatorContext();
+            Initialize(validatorContext);
+        }
+
+        public Context(string repositoryRootPath, ValidatorContext validatorContext)
+        {
+            Argument.IsNotNullOrWhitespace(() => repositoryRootPath);
+            Argument.IsNotNull(() => validatorContext);
+
+            RepositoryRootPath = repositoryRootPath;
+            ValidatorContext = validatorContext;
+
+            ValidateContext();
+
+            Initialize(validatorContext);
+        }
+
+        #endregion
+
+        #region properties
+        public string WorkingDirectory { get; set; }
+
+        public string RepositoryRootPath { get; private set; }
+
+        public ValidatorContext ValidatorContext { get; private set; }
+
+        private string ConfigFilePath { get; set; }
+        #endregion
+
+        #region Methods
+        private void Initialize(ValidatorContext validatorContext)
+        {
+            Argument.IsNotNull(() => validatorContext);
+
+            // Note: decide what directory to use
+            //WorkingDirectory = Environment.CurrentDirectory;
+            WorkingDirectory = RepositoryRootPath;
+
+            ValidatorContext = validatorContext;
+        }
+
+        /// <summary>
+        /// Validates the context.
+        /// </summary>
+        /// <exception cref="SolutionValidatorException">
+        /// Repository path '{0}' does not exist
+        /// or
+        /// Configuration file '{0}' does not exist
+        /// </exception>
+        public void ValidateContext()
+        {
+            var repositoryPath = Path.GetFullPath(RepositoryRootPath);
+
+            if (!Directory.Exists(repositoryPath))
+            {
+                throw new SolutionValidatorException("Repository path '{0}' does not exist", repositoryPath);
+            }
+
+            if (!string.IsNullOrWhiteSpace(ConfigFilePath) && !string.Equals(ConfigFilePath, SolutionValidatorEnvironment.ConfigFilePathDefaultValue))
+            {
+                ConfigFilePath = Path.GetFullPath(ConfigFilePath);
+
+                if (!File.Exists(ConfigFilePath))
+                {
+                    throw new SolutionValidatorException("Configuration file '{0}' does not exist", ConfigFilePath);
+                }
+            }
+        }
+
+        private ValidatorContext CreateValidatorContext()
+        {
+            if (!string.IsNullOrWhiteSpace(ConfigFilePath))
+            {
+                var configuration = ConfigurationHelper.Load(ConfigFilePath);
+                return configuration.ToContext();
+            }
+
+            return new ValidatorContext();
+        }
+        #endregion
+    }
+}
